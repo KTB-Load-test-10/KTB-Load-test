ad0cb2318d958471212e78e40ebb01cd77b0ffd2
SUHYUN YU <suhyun1219yu@gmail.com>
2026-08-11 11:44:14 +0900

    fix: login자동이동 제거

--- apps/frontend/pages/register.js
+++ apps/frontend/pages/register.js
@@ -54,10 +54,6 @@ const Register = () => {
       
       setSuccess(true);
       setLoading(false);
-      
-      setTimeout(() => {
-        router.push('/login');
-      }, 1000);
     } catch (err) {
       setError(err.message || '회원가입 처리 중 오류가 발생했습니다.');
       setLoading(false);
